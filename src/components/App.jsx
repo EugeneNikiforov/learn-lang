1cfe08847517dc3a42fd8858bd0f8d80af4192a7
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-10-17 23:24:19 +0300

    add appjsx

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import "../index.css";
+
+export const App = () => {
+  return (
+    <div
+      className="app-components"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 80,
+        color: "#010101",
+      }}
+    >
+      <h1>11111111</h1>
+    </div>
+  );
+};
