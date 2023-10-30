8da366b91141b16ed91508beff147174a32bc09a
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-10-30 22:39:21 +0200

    update teachersitemscss

--- src/components/TeachersItem/TeacherItem.jsx
+++ src/components/TeachersItem/TeacherItem.jsx
@@ -25,62 +25,67 @@ const TeachersItem = ({ teacher }) => {
         <img src={avatar_url} alt="teacher-pic" className={style.teachersPic} />
         <img src={onlineIcon} alt="online-pic" className={style.teachersIcon} />
       </div>
-      <div className={style.teachersName}>
-        <p className={style.teachersNameNote}>Languages</p>
-        <p className={style.teachersNamePara}>
-          {name} {surname}
-        </p>
-      </div>
-      <div className={style.teachersInfo}>
-        <ul className={style.teachersInfoList}>
-          <li>
-            <p className={style.teachersInfoPara}>Lessons online</p>
-          </li>
-          <li>
-            <p className={style.teachersInfoPara}>
-              Lessons done: {lessons_done}
+      <div className={style.teachersBlocks}>
+        <div className={style.teachersInfo}>
+          <div className={style.teachersName}>
+            <p className={style.teachersNameNote}>Languages</p>
+            <p className={style.teachersNamePara}>
+              {name} {surname}
             </p>
-          </li>
-          <li>
-            <p className={style.teachersInfoPara}>Rating: {rating}</p>
-          </li>
-          <li>
-            <p className={style.teachersInfoPara}>
-              Price / 1 hour:
-              <span className={style.teachersInfoCost}> {price_per_hour}$</span>
-            </p>
-          </li>
-        </ul>
-        <button type="button" className={style.teachersInfoBtn}></button>
-      </div>
+          </div>
+          <ul className={style.teachersInfoList}>
+            <li>
+              <p className={style.teachersInfoPara}>Lessons online</p>
+            </li>
+            <li>
+              <p className={style.teachersInfoPara}>
+                Lessons done: {lessons_done}
+              </p>
+            </li>
+            <li>
+              <p className={style.teachersInfoPara}>Rating: {rating}</p>
+            </li>
+            <li>
+              <p className={style.teachersInfoPara}>
+                Price / 1 hour:
+                <span className={style.teachersInfoCost}>
+                  {" "}
+                  {price_per_hour}$
+                </span>
+              </p>
+            </li>
+          </ul>
+          <button type="button" className={style.teachersInfoBtn}></button>
+        </div>
 
-      <div className={style.teachersDesc}>
-        <ul className={style.teachersDescList}>
-          <li>
-            <p className={style.teachersDescPara}>
-              Speaks:
-              <span className={style.teachersDescLang}> {languages}</span>
-            </p>
-          </li>
-          <li>
-            <p className={style.teachersDescPara}>
-              Lesson Info:
-              <span className={style.teachersDescSpan}> {lesson_info}</span>
-            </p>
-          </li>
-          <li>
-            <p className={style.teachersDescPara}>
-              Conditions:
-              <span className={style.teachersDescSpan}> {conditions}</span>
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div className={style.teachersLink}>
-        <p className={style.teachersLinkPara}>Read more</p>
-      </div>
-      <div>
-        <ul className={style.teachersLangLevels}>{langLevels}</ul>
+        <div className={style.teachersDesc}>
+          <ul className={style.teachersDescList}>
+            <li>
+              <p className={style.teachersDescPara}>
+                Speaks:
+                <span className={style.teachersDescLang}> {languages}</span>
+              </p>
+            </li>
+            <li>
+              <p className={style.teachersDescPara}>
+                Lesson Info:
+                <span className={style.teachersDescSpan}> {lesson_info}</span>
+              </p>
+            </li>
+            <li>
+              <p className={style.teachersDescPara}>
+                Conditions:
+                <span className={style.teachersDescSpan}> {conditions}</span>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className={style.teachersLink}>
+          <p className={style.teachersLinkPara}>Read more</p>
+        </div>
+        <div>
+          <ul className={style.teachersLangLevels}>{langLevels}</ul>
+        </div>
       </div>
     </li>
   );
